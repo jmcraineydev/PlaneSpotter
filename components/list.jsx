26555d7bd885b5f0063610d1ebd1fd5105713ddf
Jason McRainey <jmcraineydev@gmail.com>
2022-08-12 21:59:15 +0900

    feat: add initial MVP build of Aircraft list

--- components/list.jsx
+++ components/list.jsx
@@ -36,13 +36,29 @@ export default function List(props) {
         <FlatList
           data={planeList}
           renderItem={({ item }) => (
-            <View style={styles.airportBar}>
-              <Text style={styles.airportText}>{item.dep_iata}</Text>
-              <Image
-                source={require(`../assets/airplane_straight_track.png`)}
-                style={styles.listGraphic}
-              />
-              <Text style={styles.airportText}>{item.arr_iata}</Text>
+            <View style={styles.listItemContainer}>
+              <View style={styles.listItemHeader}>
+                <Text style={styles.aircraftNumber}>
+                  {item.airline_iata}: {item.reg_number}
+                </Text>
+                <Text style={styles.airportText}>{item.status}</Text>
+              </View>
+              <View style={styles.airportBar}>
+                <Text style={styles.airportText}>{item.dep_iata}</Text>
+                <Image
+                  source={require(`../assets/airplane_straight_track.png`)}
+                  style={styles.listGraphic}
+                />
+                <Text style={styles.airportText}>{item.arr_iata}</Text>
+              </View>
+              <View style={styles.flightInfoContainer}>
+                <Text style={styles.flightInfo}>ALT: {item.alt}</Text>
+                <Text style={styles.flightInfo}>SPEED: {item.speed}</Text>
+                <Text style={styles.flightInfo}>HEADING: {item.dir}</Text>
+                <Text style={styles.flightInfo}>
+                  VERT SPEED: {item.v_speed}
+                </Text>
+              </View>
             </View>
           )}
           keyExtractor={(item) => item.reg_number}
@@ -68,18 +84,44 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  aircraftNumber: {
+    fontWeight: "700",
+    fontSize: 20,
+  },
+
   airportBar: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
   airportText: {
-    fontWeight: "700",
+    fontWeight: "500",
     fontSize: 18,
     marginHorizontal: 20,
+  },
+  flightInfo: {
+    marginVertical: 1,
+    width: "50%",
+  },
+  flightInfoContainer: {
+    width: "80%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
   listGraphic: {
     width: 200,
     resizeMode: "contain",
+  },
+  listItemContainer: {
+    borderColor: "grey",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  listItemHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
