c98b5e66feb7b02f099d929f74bae814002833c8
Jason McRainey <jmcraineydev@gmail.com>
2022-08-12 23:11:29 +0900

    feat: complete inital build of Map component

--- components/map.jsx
+++ components/map.jsx
@@ -1,23 +1,6 @@
 import { StyleSheet, Image, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-// export default class Map extends Component<Props> {
-//   render() {
-//     return (
-//       <MapView
-//         style={{ flex: 1 }}
-//         region={{
-//           latitude: 42.882004,
-//           longitude: 74.582748,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//         showsUserLocation={true}
-//       />
-//     );
-//   }
-// }
-
 export default function Map(props) {
   const { aircraftFollows } = props;
   return (
@@ -48,7 +31,6 @@ export default function Map(props) {
           ))
         : ""}
     </MapView>
-    // <Text>MAP Container</Text>
   );
 }
 
@@ -57,24 +39,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignContent: "center",
-    // width: 450,
-    // paddingTop: 25,
-    // height: 25,
-    // borderColor: "black",
-    // borderWidth: 1,
-    // backgroundColor: "#FFFFFF",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   mapMarker: {
     height: 30,
     width: 30,
   },
 });
-
-<Marker coordinate={{ latitude: 38.71, longitude: 140.46 }} title={"JA743A"}>
-  <Image
-    source={require("../assets/markericon.png")}
-    style={styles.mapMarker}
-  />
-</Marker>;
