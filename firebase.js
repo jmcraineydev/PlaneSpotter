1469a23f4cb80171361a542a0e81af93bdb10a57
Jason McRainey <jmcraineydev@gmail.com>
2022-08-13 10:14:27 +0900

    REFACTOR: remove unused code

--- firebase.js
+++ firebase.js
@@ -1,16 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "@firebase/auth";
-// import {
-//   REACT_APP_FB_APIKEY,
-//   REACT_APP_FB_AUTHDOMAIN,
-//   REACT_APP_FB_PROJECTID,
-//   REACT_APP_FB_STORAGEBUCKET,
-//   REACT_APP_FB_MESSAGESENDERID,
-//   REACT_APP_FB_APPID,
-// } from "@env";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
